52b5a18c5b6caadf0102b93fd320e57bb2cb702e
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2022-08-10 06:14:22 +0530

    Modified about page

--- src/main/java/controller/AboutFormController.java
+++ src/main/java/controller/AboutFormController.java
@@ -14,26 +14,12 @@ public class AboutFormController {
     public AnchorPane ctrAbout;
 
     public void initialize(){
-        RotateTransition rt = new RotateTransition(Duration.millis(750), ctrAbout);
-        rt.setFromAngle(0);
-        rt.setToAngle(270);
-        rt.playFromStart();
-//
-//        TranslateTransition tt = new TranslateTransition(Duration.millis(500), imgRose);
-//        tt.setFromX(-500);
-//        tt.setToX(50);
-//        tt.playFromStart();
-//
-//        FadeTransition ft = new FadeTransition(Duration.millis(1500), ctrAbout);
-//        ft.setFromValue(0);
-//        ft.setToValue(1);
-//        ft.playFromStart();
-//
-//        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(500), imgRose);
-//        scaleTransition.setFromX(0);
-//        scaleTransition.setFromY(0);
-//        scaleTransition.setToX(1);
-//        scaleTransition.setToY(1);
-//        scaleTransition.playFromStart();
+
+        FadeTransition ft = new FadeTransition(Duration.millis(1500), ctrAbout);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        ft.playFromStart();
+
+
     }
 }
